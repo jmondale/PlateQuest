186de4bb5aacce012f4d76d33d0d98cdeea47b15
Jaye Mondale <jmondale@gmail.com>
2024-08-18 21:23:21 -0600

    Initial code

---
+++ PlateQuest/PlateQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateQuestApp.swift
+//  PlateQuest
+//
+//  Created by Jaye Mondale on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PlateQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
